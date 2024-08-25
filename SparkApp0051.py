f0bb99ccf14d2ce89640c0506817170b5a4daa87
cesaramehri <cesar.amehri@gmail.com>
2024-08-25 13:56:44 +1200

    Spark App 005: MongoDB Integration

---
+++ SparkApp0051.py
@@ -0,0 +1,32 @@
+### Read data from a MongoDB Table ###
+
+# Import Packages
+from pyspark.sql import SparkSession, Row, functions
+
+# Main function
+if __name__ == "__main__":
+    # Create s Spark Session
+    spark = SparkSession.builder\
+                        .appName("MongoDBIntegration")\
+                        .getOrCreate()
+
+    # Read data from MongoDB table into a Dataframe
+    readUser = spark.read\
+                     .format("com.mongodb.spark.sql.DefaultSource")\
+                     .option("uri","mongodb://127.0.0.1/moviesdata.user")\
+                     .load()
+    
+    # Create a view on the df 
+    readUser.createOrReplaceTempView("user")
+
+    # Explore the df
+    readUser.printSchema()
+
+    # Query the df using sql
+    sqlDF = spark.sql("""
+                        SELECT occupation, count(user_id) as cnt_usr
+                        FROM user
+                        GROUP BY occupation
+                        ORDER BY cnt_user DESC
+                      """)
+    sqlDF.show()
